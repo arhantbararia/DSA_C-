fe47a692a41ade73b2386df2102b44edc6e1f899
arhantbararia <arhantbararia@gmail.com>
2021-09-30 15:19:22 +0530

    3.2 done

---
+++ Chapter 1/EX01-2.cpp
@@ -0,0 +1,47 @@
+
+
+
+
+
+class node
+{
+	int data;
+	node* next;
+	
+};
+
+
+
+void swap(node n1 , node n2)
+{
+	n1.next = n2.next;
+	n2.next = n2.next.next;
+	n2.next.next = n2;
+	
+	
+}
+
+
+class node{
+	
+	int data;
+	node* next;
+	node *prev;
+};
+
+/// a-b-c- -- (n1-1)-n1-n2-(n2+1)---- /// to swap n1 , n2 
+void swap(node n1 , node n2)
+{
+	n1.prev.next = n2;
+	n2.prev = n1.prev;
+	
+	n1.next = n2.next;
+	n2.next.prev = n1;
+	
+	n2.next = n1;
+	n1.prev = n2;
+	
+	
+	n1.prev = n2;
+	n2.next = n1;
+}
