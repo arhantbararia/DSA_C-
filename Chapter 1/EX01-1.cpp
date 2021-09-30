34759e2528a39a23f6a60cd5239d141306c14441
Arhant Bararia <61796574+arhantbararia@users.noreply.github.com>
2021-09-30 14:08:31 +0530

    Updated 3.1

--- Chapter 1/EX01-1.cpp
+++ Chapter 1/EX01-1.cpp
@@ -19,7 +19,7 @@ void printLots(list<int> L , list<int> P)
     list<int> :: iterator it1;
     list<int> :: iterator it2;
 
-    int i = 0;
+   
 	
 	
 	/*
@@ -31,6 +31,8 @@ void printLots(list<int> L , list<int> P)
 	*/
     for(it1 = P.begin() ; it1 != P.end() ; ++it1)
     {
+    int i = 0;
+	
         for(it2 = L.begin(); it2 != L.end(); it2++ )
         {
             if(i == *it1 )
